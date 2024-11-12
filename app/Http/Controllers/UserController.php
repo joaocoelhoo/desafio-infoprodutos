678911d8f2ea094ebc860dc247c0cfd8b6f72d56
Joao Coelho <joaopaulomelocoelho@gmail.com>
2024-11-12 20:22:20 -0300

    Add user CRUD

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $user = User::all();
+        return response()->json([
+            'status' => true,
+            'message' => 'User retrieved successfully',
+            'data' => $user
+        ], 200);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'string|max:255',
+            'email' => 'string|email|max:255|unique:users',
+            'password' => 'string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation error',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $user = User::findOrFail($id);
+        $user->update($request->all());
+
+        return response()->json([
+            'status' => true,
+            'message' => 'User updated successfully',
+            'data' => $user
+        ], 200);
+    }
+
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        
+        return response()->json([
+            'status' => true,
+            'message' => 'User deleted successfully'
+        ], 204);
+    }
+}
